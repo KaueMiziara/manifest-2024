51318b0f421d18fdedb8f4a36f837100dec2bed8
Kaue Miziara <kauemiziara@gmail.com>
2024-02-08 18:09:09 -0300

    feat: create AboutProjectSection component

--- app/src/main/java/org/manifest/manifest24/ui/pages/HomePage.kt
+++ app/src/main/java/org/manifest/manifest24/ui/pages/HomePage.kt
@@ -1,21 +1,32 @@
 package org.manifest.manifest24.ui.pages
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.manifest.manifest24.ui.components.ManifestAppBar
+import org.manifest.manifest24.ui.components.home_page.AboutProjectSection
 
 @Composable
 fun HomePage() {
-    Scaffold(
-        topBar = {
-            ManifestAppBar()
-        }
-    ) { innerPadding ->
-        Column(Modifier.padding(innerPadding)) {
+    Scaffold(topBar = {
+        ManifestAppBar()
+    }) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.spacedBy(32.dp),
+        ) {
+            AboutProjectSection()
+            PlaceholderPage()
             PlaceholderPage()
         }
     }
