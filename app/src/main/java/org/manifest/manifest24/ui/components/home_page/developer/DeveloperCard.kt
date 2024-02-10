01e6dbb7bf9605f3f9f01338e4f9af7082e813ff
Kaue Miziara <kauemiziara@gmail.com>
2024-02-10 20:04:11 -0300

    ui: adjust DeveloperCard content alignment

--- app/src/main/java/org/manifest/manifest24/ui/components/home_page/developer/DeveloperCard.kt
+++ app/src/main/java/org/manifest/manifest24/ui/components/home_page/developer/DeveloperCard.kt
@@ -1,5 +1,6 @@
 package org.manifest.manifest24.ui.components.home_page.developer
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -28,6 +30,7 @@ fun DeveloperCard(
         ),
     ) {
         Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .padding(16.dp)
@@ -37,15 +40,21 @@ fun DeveloperCard(
                 Icons.Rounded.AccountCircle,
                 contentDescription = "Profile picture placeholder",
                 tint = Color.White,
-                modifier = Modifier.size(64.dp)
+                modifier = Modifier.size(64.dp),
             )
-            Text(color = Color.White, text = name)
             Text(
                 color = Color.White,
+                textAlign = TextAlign.Center,
+                text = name,
+            )
+            Text(
+                color = Color.White,
+                textAlign = TextAlign.Center,
                 text = stacks.joinToString(", "),
             )
             Text(
                 color = Color.White,
+                textAlign = TextAlign.Center,
                 text = languages.joinToString(", "),
             )
         }
