ccb89f31d86d2f630d6ddadb5f953a074d492ead
Kaue Miziara <kauemiziara@gmail.com>
2024-02-09 21:29:19 -0300

    feat: add 'Meet the Team' text to DevelopersSection

--- app/src/main/java/org/manifest/manifest24/ui/components/home_page/developer/DevelopersSection.kt
+++ app/src/main/java/org/manifest/manifest24/ui/components/home_page/developer/DevelopersSection.kt
@@ -1,19 +1,32 @@
 package org.manifest.manifest24.ui.components.home_page.developer
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun DevelopersSection() {
-    Row(
-        horizontalArrangement = Arrangement.spacedBy(16.dp),
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        DeveloperCard()
-        DeveloperCard()
+        Text(
+            fontWeight = FontWeight.Bold,
+            text = "Meet the Team",
+        )
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            DeveloperCard()
+            DeveloperCard()
+        }
     }
 }
 
