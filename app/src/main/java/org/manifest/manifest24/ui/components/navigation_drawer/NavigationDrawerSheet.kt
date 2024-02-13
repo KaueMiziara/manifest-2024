96b9f00bb2443f648194e707c3d522140ea138ba
Kaue Miziara <kauemiziara@gmail.com>
2024-02-12 22:24:47 -0300

    refactor: move NavigationItems list to NavigationDrawer

--- app/src/main/java/org/manifest/manifest24/ui/components/navigation_drawer/NavigationDrawerSheet.kt
+++ app/src/main/java/org/manifest/manifest24/ui/components/navigation_drawer/NavigationDrawerSheet.kt
@@ -1,8 +1,5 @@
 package org.manifest.manifest24.ui.components.navigation_drawer
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalDrawerSheet
@@ -17,7 +14,6 @@ import androidx.compose.runtime.setValue
 import androidx.navigation.NavController
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
-import org.manifest.manifest24.ui.navigation.NavRoute
 import org.manifest.manifest24.ui.navigation.NavigationItem
 
 @Composable
@@ -25,18 +21,14 @@ fun NavigationDrawerSheet(
     scope: CoroutineScope = rememberCoroutineScope(),
     drawerState: DrawerState,
     navController: NavController,
+    navItems: List<NavigationItem>,
 ) {
-    val items = listOf(
-        NavigationItem("Home", NavRoute.Home.route, Icons.Default.Home),
-        NavigationItem("Placeholder1", NavRoute.Placeholder.route, Icons.Default.Warning),
-    )
-
     var selectedItemIndex by rememberSaveable {
         mutableIntStateOf(0)
     }
 
     ModalDrawerSheet {
-        items.forEachIndexed { index, item ->
+        navItems.forEachIndexed { index, item ->
             NavigationDrawerItem(
                 label = { Text(item.title) },
                 selected = index == selectedItemIndex,
