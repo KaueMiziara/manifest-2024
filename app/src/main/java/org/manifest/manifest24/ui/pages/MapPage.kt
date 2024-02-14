11a5da78d7cec449c1209772c3d65db8e930d029
Kaue Miziara <kauemiziara@gmail.com>
2024-02-13 22:23:52 -0300

    feat: add initial OpenStreetMap composable to MapPage

--- app/src/main/java/org/manifest/manifest24/ui/pages/MapPage.kt
+++ app/src/main/java/org/manifest/manifest24/ui/pages/MapPage.kt
@@ -1,25 +1,33 @@
 package org.manifest.manifest24.ui.pages
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.utsman.osmandcompose.OpenStreetMap
+import com.utsman.osmandcompose.rememberCameraState
 import org.manifest.manifest24.ui.components.ManifestAppBar
+import org.osmdroid.util.GeoPoint
 
 @Composable
 fun MapPage(drawerState: DrawerState) {
     Scaffold(
         topBar = { ManifestAppBar(drawerState = drawerState) }
     ) {
-        Column(modifier = Modifier.padding(it)) {
-            Text(text = "Map Page Placeholder")
-        }
+        OpenStreetMap(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize(),
+            cameraState = rememberCameraState {
+                geoPoint = GeoPoint(-23.57309181123147, -46.623389398907634)
+                zoom = 17.0
+            }
+        )
     }
 }
 
