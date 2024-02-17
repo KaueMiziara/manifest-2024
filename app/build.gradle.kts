668822d8b4f3e2f05cc390f57e36c0d66d3967eb
Kaue Miziara <kauemiziara@gmail.com>
2024-02-17 17:58:09 -0300

    build: add Retrofit and OkHttp3 dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,4 +75,10 @@ dependencies {
     // OSMdroid
     implementation("org.osmdroid:osmdroid-android:6.1.16")
     implementation("tech.utsmankece:osm-android-compose:0.0.5")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
 }
