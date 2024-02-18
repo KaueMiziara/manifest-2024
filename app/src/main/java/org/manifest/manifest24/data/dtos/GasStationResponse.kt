bfe84a0e4471394662665b6015394c09f5781196
Kaue Miziara <kauemiziara@gmail.com>
2024-02-18 15:28:27 -0300

    refactor: change GasStationResponse fields names to match the API's and turn it into a data class

--- app/src/main/java/org/manifest/manifest24/data/dtos/GasStationResponse.kt
+++ app/src/main/java/org/manifest/manifest24/data/dtos/GasStationResponse.kt
@@ -3,15 +3,15 @@ package org.manifest.manifest24.data.dtos
 import org.manifest.manifest24.data.models.review.GasStation
 import org.osmdroid.util.GeoPoint
 
-class GasStationResponse(
-    private val id: Long,
-    private val address: String,
-    private val latitude: Double,
-    private val longitude: Double,
+data class GasStationResponse(
+    val id: Long,
+    val address: String,
+    val lat: Double,
+    val lng: Double,
 ) {
     fun toGasStation() = GasStation(
         id = id,
         address = address,
-        geoPoint = GeoPoint(latitude, longitude),
+        geoPoint = GeoPoint(lat, lng),
     )
 }
