e18e9fc45ddf017d84d39ae1a853f7520139e14d
Kaue Miziara <kauemiziara@gmail.com>
2024-02-18 15:14:47 -0300

    refactor: add id field to GasStationResponse DTO and change id type in GasStation model to Long

--- app/src/main/java/org/manifest/manifest24/data/dtos/GasStationResponse.kt
+++ app/src/main/java/org/manifest/manifest24/data/dtos/GasStationResponse.kt
@@ -4,12 +4,13 @@ import org.manifest.manifest24.data.models.review.GasStation
 import org.osmdroid.util.GeoPoint
 
 class GasStationResponse(
+    private val id: Long,
     private val address: String,
     private val latitude: Double,
     private val longitude: Double,
 ) {
     fun toGasStation() = GasStation(
-        id = 0,
+        id = id,
         address = address,
         geoPoint = GeoPoint(latitude, longitude),
     )
--- app/src/main/java/org/manifest/manifest24/data/models/review/GasStation.kt
+++ app/src/main/java/org/manifest/manifest24/data/models/review/GasStation.kt
@@ -3,7 +3,7 @@ package org.manifest.manifest24.data.models.review
 import org.osmdroid.util.GeoPoint
 
 data class GasStation(
-    val id: Int,
+    val id: Long,
     val address: String,
     val geoPoint: GeoPoint,
 )
