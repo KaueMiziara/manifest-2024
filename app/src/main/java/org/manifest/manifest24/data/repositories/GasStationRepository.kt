b19eda6142c11424e33ea79de07e070335182243
Kaue Miziara <kauemiziara@gmail.com>
2024-02-21 21:17:53 -0300

    docs: add documentation comments to GasStationRepository interface and implementation classes

--- app/src/main/java/org/manifest/manifest24/data/repositories/GasStationRepository.kt
+++ app/src/main/java/org/manifest/manifest24/data/repositories/GasStationRepository.kt
@@ -6,6 +6,11 @@ import org.manifest.manifest24.data.dtos.GasStationResponse
 import org.manifest.manifest24.network.GasStationApiService
 import javax.inject.Inject
 
+/**
+ * Implementation of the IGasStationRepository interface for retrieving gas station data from the API.
+ *
+ * @property apiService GasStationApiService for making API requests.
+ */
 class GasStationRepository @Inject constructor(
     private val apiService: GasStationApiService,
 ) : IGasStationRepository {
--- app/src/main/java/org/manifest/manifest24/data/repositories/IGasStationRepository.kt
+++ app/src/main/java/org/manifest/manifest24/data/repositories/IGasStationRepository.kt
@@ -3,7 +3,18 @@ package org.manifest.manifest24.data.repositories
 import kotlinx.coroutines.flow.Flow
 import org.manifest.manifest24.data.dtos.GasStationResponse
 
+/**
+ * Interface defining operations for retrieving gas station data.
+ */
 interface IGasStationRepository {
+    /**
+     * Retrieves a flow of gas stations nearest to a specified location.
+     *
+     * @param limit Maximum number of gas stations to retrieve.
+     * @param lat Latitude of the reference location.
+     * @param lng Longitude of the reference location.
+     * @return Flow emitting a list of GasStationResponse objects representing the nearest gas stations.
+     */
     suspend fun findGasStationsNearestTo(
         limit: Int,
         lat: Double,
